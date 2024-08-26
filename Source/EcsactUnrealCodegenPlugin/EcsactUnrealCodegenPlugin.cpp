2d35bdb18bd957e149ef4daf7a210233c6df95e4
Ezekiel Warren <ezekiel@seaube.com>
2024-08-26 11:14:56 -0700

    feat: component ustructs

--- Source/EcsactUnrealCodegenPlugin/EcsactUnrealCodegenPlugin.cpp
+++ Source/EcsactUnrealCodegenPlugin/EcsactUnrealCodegenPlugin.cpp
@@ -52,7 +52,7 @@ auto ecsact_decl_name_to_pascal(const std::string& input) -> std::string {
 	auto capitalize_next = true;
 
 	for(char ch : input) {
-		if(ch == '.') {
+		if(ch == '.' || ch == '_') {
 			capitalize_next = true; // Capitalize the next character after a separator
 		} else if(capitalize_next) {
 			result << static_cast<char>(std::toupper(ch));
@@ -93,6 +93,148 @@ auto ecsact_codegen_plugin_name() -> const char* {
 	return "Unreal";
 }
 
+static auto ecsact_type_to_unreal_type(
+	ecsact::codegen_plugin_context& ctx,
+	ecsact_builtin_type             type
+) -> std::string {
+	switch(type) {
+		case ECSACT_BOOL:
+			return "bool";
+		case ECSACT_I8:
+		case ECSACT_I16:
+		case ECSACT_I32:
+			return "int32";
+		case ECSACT_U8:
+		case ECSACT_U16:
+		case ECSACT_U32:
+			return "uint32";
+		case ECSACT_F32:
+			return "float";
+		case ECSACT_ENTITY_TYPE:
+			break;
+		default:
+			ctx.fatal(
+				"Ecsact Unreal codegen plugin unknown builtin type ({}). Cannot "
+				"proceed. Are you using the correct version of the Ecsact SDK with the "
+				"correct version of the Ecsact Unreal codegen plugin?",
+				static_cast<int>(type)
+			);
+			break;
+	}
+
+	return "";
+}
+
+static auto ecsact_type_to_unreal_type(
+	ecsact::codegen_plugin_context& ctx,
+	ecsact_field_type               type
+) -> std::string {
+	switch(type.kind) {
+		case ECSACT_TYPE_KIND_BUILTIN:
+			if(type.length == 1) {
+				return ecsact_type_to_unreal_type(ctx, type.type.builtin);
+			}
+			ctx.fatal("Ecsact builtin array fields not supported yet");
+			break;
+		case ECSACT_TYPE_KIND_ENUM:
+			ctx.fatal("Esact enum type not supported in yet");
+			break;
+		case ECSACT_TYPE_KIND_FIELD_INDEX:
+			ctx.fatal("Ecsact field index not supported in unreal yet");
+			break;
+		default:
+			ctx.fatal(
+				"Ecsact Unreal codegen plugin unknown field type (kind={}). Cannot "
+				"proceed. Are you using the correct version of the Ecsact SDK with the "
+				"correct version of the Ecsact Unreal codegen plugin?",
+				static_cast<int>(type.kind)
+			);
+			break;
+	}
+
+	return "";
+}
+
+static auto ecsact_ustruct_name(auto decl_id) -> std::string {
+	auto name = ecsact::meta::decl_full_name(decl_id);
+	auto pascal_name = ecsact_decl_name_to_pascal(name);
+	return std::format("F{}", pascal_name);
+}
+
+template<typename T>
+static auto uproperty_clamp_min_max() -> std::string {
+	return std::format(
+		R"(, Meta = (ClampMin = "{}", ClampMax = "{}"))",
+		std::numeric_limits<T>::min(),
+		std::numeric_limits<T>::max()
+	);
+}
+
+static auto print_ecsact_type_uproperty(
+	ecsact::codegen_plugin_context& ctx,
+	ecsact_field_type               field_type
+) -> void {
+	ctx.write("UPROPERTY(EditAnywhere, BlueprintReadWrite");
+	switch(field_type.kind) {
+		case ECSACT_TYPE_KIND_BUILTIN:
+			switch(field_type.type.builtin) {
+				case ECSACT_BOOL:
+					break;
+				case ECSACT_I8:
+					ctx.write(uproperty_clamp_min_max<int8_t>());
+					break;
+				case ECSACT_U8:
+					ctx.write(uproperty_clamp_min_max<uint8_t>());
+					break;
+				case ECSACT_I16:
+					ctx.write(uproperty_clamp_min_max<int16_t>());
+					break;
+				case ECSACT_U16:
+					ctx.write(uproperty_clamp_min_max<uint16_t>());
+					break;
+				case ECSACT_I32:
+					break;
+				case ECSACT_U32:
+					break;
+				case ECSACT_F32:
+					break;
+				case ECSACT_ENTITY_TYPE:
+					break;
+			}
+			break;
+		case ECSACT_TYPE_KIND_ENUM:
+			break;
+		case ECSACT_TYPE_KIND_FIELD_INDEX:
+			break;
+	}
+
+	ctx.write(")\n");
+}
+
+static auto print_ustruct(ecsact::codegen_plugin_context& ctx, auto in_compo_id)
+	-> void {
+	auto compo_id = ecsact_id_cast<ecsact_composite_id>(in_compo_id);
+	auto compo_name = ecsact::meta::decl_full_name(compo_id);
+	auto compo_pascal_name = ecsact_ustruct_name(compo_id);
+
+	ctx.writef("USTRUCT(BlueprintType)");
+	block(ctx, std::format("struct {}", compo_pascal_name), [&] {
+		ctx.writef("GENERATED_BODY()\n\n");
+		auto fields = ecsact::meta::get_field_ids(compo_id);
+
+		for(auto field_id : fields) {
+			auto field_type = ecsact::meta::get_field_type(compo_id, field_id);
+			auto field_unreal_type = ecsact_type_to_unreal_type(ctx, field_type);
+			auto field_name = ecsact::meta::field_name(compo_id, field_id);
+			auto field_pascal_name = ecsact_decl_name_to_pascal(field_name);
+
+			print_ecsact_type_uproperty(ctx, field_type);
+			ctx.write(std::format("{} {};\n", field_unreal_type, field_pascal_name));
+		}
+	});
+	ctx.writef(";\n\n");
+}
+
 static auto generate_header(ecsact::codegen_plugin_context ctx) -> void {
 	ctx.writef("#pragma once\n\n");
 
@@ -106,6 +248,10 @@ static auto generate_header(ecsact::codegen_plugin_context ctx) -> void {
 		ecsact_decl_name_to_pascal(ecsact::meta::package_name(ctx.package_id));
 
 	ctx.writef("\n\n");
+
+	for(auto comp_id : ecsact::meta::get_component_ids(ctx.package_id)) {
+		print_ustruct(ctx, comp_id);
+	}
 
 	ctx.write(std::format(
 		"UCLASS(Blueprintable, meta = "
@@ -131,16 +277,22 @@ static auto generate_header(ecsact::codegen_plugin_context ctx) -> void {
 				auto comp_type_cpp_name = cpp_identifier(comp_full_name);
 				auto comp_name = ecsact::meta::component_name(comp_id);
 				auto comp_pascal_name = ecsact_decl_name_to_pascal(comp_name);
+				auto comp_ustruct_name = ecsact_ustruct_name(comp_id);
 				ctx.write(std::format(
 					"UFUNCTION(BlueprintNativeEvent, Category = \"Ecsact Runner\", meta "
 					"= "
 					"(DisplayName = \"Init {}\"))\n",
 					comp_full_name
 				));
-				ctx.write(std::format("void Init{}();\n", comp_pascal_name));
 				ctx.write(std::format(
-					"virtual void Init{}_Implementation();\n",
-					comp_pascal_name
+					"void Init{0}(int32 Entity, {1} {0});\n",
+					comp_pascal_name,
+					comp_ustruct_name
+				));
+				ctx.write(std::format(
+					"virtual void Init{0}_Implementation(int32 Entity, {1} {0});\n",
+					comp_pascal_name,
+					comp_ustruct_name
 				));
 			}
 		}
@@ -158,13 +310,16 @@ static auto generate_source(ecsact::codegen_plugin_context ctx) -> void {
 		auto comp_full_name = ecsact::meta::decl_full_name(comp_id);
 		auto comp_name = ecsact::meta::component_name(comp_id);
 		auto comp_pascal_name = ecsact_decl_name_to_pascal(comp_name);
+		auto comp_ustruct_name = ecsact_ustruct_name(comp_id);
 
 		block(
 			ctx,
 			std::format(
-				"void U{}EcsactRunnerSubsystem::Init{}_Implementation()",
+				"void U{0}EcsactRunnerSubsystem::Init{1}_Implementation"
+				"(int32 Entity, {2} {1})",
 				package_pascal_name,
-				comp_pascal_name
+				comp_pascal_name,
+				comp_ustruct_name
 			),
 			[&] {
 
